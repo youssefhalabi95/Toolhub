03a58a596c88e7f80436ae111576d42a5b669624
Youssef Halabi <youssef.halabi.95@gmail.com>
2026-05-30 03:00:21 +0300

    Fix syntax error in app startup

--- app.py
+++ app.py
@@ -83,5 +83,5 @@ def contact():
 
 if __name__ == "__main__":
     import os
-    port = int(os.egit add app.pynviron.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
