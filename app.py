f928d006f36631136770d2ec5cc5c7e36acb7e84
Youssef Halabi <youssef.halabi.95@gmail.com>
2026-05-31 23:20:06 +0300

    Prepare deployment for Railway

--- app.py
+++ app.py
@@ -87,5 +87,10 @@ def contact():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
